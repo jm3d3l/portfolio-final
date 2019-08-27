a9acf026117ca0d304eff93e73c218febafb01b6
j0hnm3d3l <johnmedel.mirafuentes@gmail.com>
2019-08-27 12:54:08 +0800

    adding image in image view

--- src/common/image-view/index.jsx
+++ src/common/image-view/index.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import LandingPage from '../../images/home.JPG';
+import Browse from '../../images/browse.JPG';
+import Items from '../../images/item.JPG';
 import './style.scss'
 const ImageViewer = () => {
     return (
         <div className="image-wrapper">
             <div className="image-viewer-container">
-                <div className="image img1">image 1</div>
+                {/* <div className="image img1">image 1</div> 
                 <div className="image img2">image 2</div>
-                <div className="image img3">image 3</div>
+                <div className="image img3">image 3</div>*/}
+                <img src={LandingPage} className="image img1" />
+                <img src={Browse} className="image img2" />
+                <img src={Items} className="image img3" />
+
             </div>
         </div>
     );
--- src/common/project-details/grid-view.jsx
+++ src/common/project-details/grid-view.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Grid from '@material-ui/core/Grid';
+
 
 const GridView = ({ item1, item2 }) => {
     return (
